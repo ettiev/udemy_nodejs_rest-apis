82318bd00b086a357c849f12be7af37078ce140f
ettiev <126219891+ettiev@users.noreply.github.com>
2023-11-27 12:14:55 +0200

    Add Login functionality password hashing and salting and JSON web tokens
    Added login route and controller
    Install bcrypt and using bcrypt for hashing and salting passwords
    Install jsonwebtoken to be able to authenticate users

--- routes/auth.js
+++ routes/auth.js
@@ -7,15 +7,21 @@ const User = require("../models/user");
 const router = express.Router();
 
 router.put("/signup", [
-    body('email').isEmail().withMessage("Please enter a valid email!").custom((value, { req }) => {
-            User.findOne({email: value}).then(userDoc => {
+    body('email')
+    .isEmail()
+    .withMessage("Please enter a valid email!")
+    .custom((value, { req }) => {
+            return User.findOne({ email: value }).then(userDoc => {
                 if (userDoc) {
                     return Promise.reject("E-mail address already exists!");
                 }
             })
-        }).normalizeEmail(),
+        })
+    .normalizeEmail(),
     body('password').trim().isLength({min: 5}),
     body('name').trim().not().isEmpty()
 ], authController.signup);
+
+router.post("/login", authController.login);
 
 module.exports = router;
