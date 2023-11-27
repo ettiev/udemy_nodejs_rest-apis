4cb8c93d69485043155f87dba6dec49b205d72ec
ettiev <126219891+ettiev@users.noreply.github.com>
2023-11-27 13:03:08 +0200

    Added isAuth Middleware
    Check all routes that user is authenticated with isAuth middleware.

--- routes/feed.js
+++ routes/feed.js
@@ -1,27 +1,28 @@
 const express = require('express');
 const { body } = require("express-validator");
+const isAuth = require("../middleware/isAuth");
 
 const feedController = require("../controllers/feed");
 
 const router = express.Router();
 
 //  /feed/posts
-router.get("/posts", feedController.getPosts);
+router.get("/posts", isAuth, feedController.getPosts);
 
 //  /feed/post
-router.post("/post", [
+router.post("/post", isAuth, [
     body("title").trim().isLength({min: 5}),
     body("content").trim().isLength({min: 5})
 ], feedController.createPost);
 
 // /feed/post/[postId]
-router.get("/post/:postId", feedController.getPost);
+router.get("/post/:postId", isAuth, feedController.getPost);
 
-router.put("/post/:postId", [
+router.put("/post/:postId", isAuth, [
     body("title").trim().isLength({min: 5}),
     body("content").trim().isLength({min: 5})
 ], feedController.updatePost)
 
-router.delete("/post/:postId", feedController.deletePost);
+router.delete("/post/:postId", isAuth, feedController.deletePost);
 
 module.exports = router;
