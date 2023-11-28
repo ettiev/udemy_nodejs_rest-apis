a0f21fac899079de3acdea1131340db64a0f966c
ettiev <126219891+ettiev@users.noreply.github.com>
2023-11-28 18:03:50 +0200

    better error handing and login functionality
    installed validator
    fixed login functionality
    user signup data validation
    improved error handling

--- app.js
+++ app.js
@@ -39,24 +39,39 @@ app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+    res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, PATCH, DELETE");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+    }
     next();
 });
 
 app.use("/graphql", graphqlHTTP({
     schema: graphqlSchema,
     rootValue: graphqlResolver,
-    graphiql: true
+    graphiql: true,
+    customFormatErrorFn(err) {                   
+        if (!err.originalError){
+            return err;
+        }
+        const data = err.originalError.data;
+        const message = err.message || "An error occured!";
+        const code = err.originalError.code || 500;
+        return {
+            message: message,
+            status: code,
+            data: data
+        };   
+    }
 }));
 
-app.use((error, req, res, next) => {
-    console.log(error);
-    const status = error.statusCode || 500;
-    const message = error.message;
-    const data = error.data;
-    res.status(status).json({ message: message, data: data });
-})
+// app.use((error, req, res, next) => {             // Alternative code
+//     const status = error.statusCode || 500;
+//     const message = error.message;
+//     const data = error.data;
+//     res.status(status).json({ message: message, data: data });
+// })
 
 const MONGOOSE_URI = process.env.MONGOOSE_URI;
 mongoose.connect(MONGOOSE_URI)
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,13 +1,33 @@
 const bcrypt = require("bcryptjs");
+const validator = require("validator");
+const jwt = require("jsonwebtoken");
+require("dotenv").config;
 
 const User = require("../models/user");
 
 module.exports = {
     createUser: async function({ userInput }, req) {
         //const email = args.userInput.email;
+        const errors = [];
+        if (!validator.isEmail(userInput.email)) {
+            errors.push({message: "Email is invalid!"})
+        }
+        if (validator.isEmpty(userInput.password) || !validator.isLength(userInput.password, {min : 5})) {
+            errors.push({message: "Password is too short!"})
+        }
+        if (validator.isEmpty(userInput.name) || !validator.isLength(userInput.name, {min : 2})) {
+            errors.push({message: "Name is too short!"})
+        }
+        console.log(errors);
+        if (errors.length > 0) {
+            const error = new Error("Invalid input!");
+            throw error
+        }
         const existingUser = await User.findOne({email: userInput.email});
         if (existingUser) {
             const error = new Error("User exists already!");
+            error.data = errors;
+            error.code = 422;
             throw error;
         }
         const hashedPwd = await bcrypt.hash(userInput.password, 12);
@@ -21,5 +41,26 @@ module.exports = {
             ...createdUser._doc, 
             _id: createdUser._id.toString()
         };
+    },
+    login: async function({ email, password }) {
+        const user = await User.findOne({email: email});
+        if (!user) {
+            const error = new Error("User not found!");
+            error.code = 401;
+            throw error;
+        }
+        const isEqual = await bcrypt.compare(password, user.password);
+        if (!isEqual) {
+            const error = new Error("Password is incorrect!");
+            error.code = 401;
+            throw error;
+        }
+        const token = jwt.sign({
+            userId: user._id.toString(),
+            email: user.email
+            }, process.env.secretToken,
+            { expiresIn: "1h" }
+        );
+        return { token: token, userId: user._id.toString() };
     }    
 };
